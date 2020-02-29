22e0c96cea45ccf776160b740ba7c4fe124640e1
Terril Thomas <terril.thomas97@gmail.com>
2020-02-29 18:43:44 +0800

    First successfull run for the flutter code

--- android/app/src/main/kotlin/bookingfields/field_app/MainActivity.kt
+++ android/app/src/main/kotlin/bookingfields/field_app/MainActivity.kt
@@ -1,12 +1,14 @@
 package bookingfields.field_app
 
+import android.os.Bundle
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.app.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
     }
 }
