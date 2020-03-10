68c91dc423a8e942247fee102106b07920a21213
Terril Thomas <vente9@sg.uob>
2020-03-10 16:04:08 +0800

    Added Fields Details Page from Home

---
+++ lib/pages/field_details_page.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+
+class FieldsDetailsPage extends StatefulWidget {
+
+  @override
+  _DetailsPageState createState() {
+    return _DetailsPageState();
+  }
+}
+
+class _DetailsPageState extends State<FieldsDetailsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
